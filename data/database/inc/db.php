91830bb07f761a3c909a90fefeac65375131fb2a
Erik Wiegert <WiegerE2@basfad.basf.net>
2019-07-30 11:46:34 +0200

    another database test

--- data/database/inc/db.php
+++ data/database/inc/db.php
@@ -1,6 +1,12 @@
 <?php
 echo date("H:i:s");
-?>
+
 $dbh = mysql_connect("rdbms.strato.de", "U3846798", "Datenbank1");
 $query = "use DB3846798";
 if (!mysql_query($query, $dbh)) die("Datenbank existiert nicht.");
+
+<FilesMatch \.php$>
+SetHandler application/x-httpd-php
+</FilesMatch>
+
+?>
